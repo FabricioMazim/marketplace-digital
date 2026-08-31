8060600541dd51711dd77f5b3b3f9981e33e3780
Fabricio Borges Mazim <fabricio.bmazim@gmail.com>
2026-08-31 20:31:08 -0300

    Add product detail page

--- products/views.py
+++ products/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
-# Create your views here.
+
+def product_detail(request, product_id):
+    product = get_object_or_404(
+        Product,
+        id=product_id,
+        is_active=True
+    )
+
+    return render(
+        request,
+        "products/product_detail.html",
+        {
+            "product": product
+        }
+    )
